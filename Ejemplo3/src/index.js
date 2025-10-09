366adf5c50bd8e85a5c7e3e7ba4379bc30d7d508
iruuuuu <irenemjh13@gmail.com>
2025-10-09 14:36:27 +0200

    Ejemplo3 por repasar

---
+++ Ejemplo3/src/index.js
@@ -0,0 +1,10 @@
+// importaciones
+
+import { getAllUser } from "./helpers/ejemplos.js";
+
+// inicio aplicacion    destructuring por defecto
+
+console.log("1. --Test de traer todos los usuarios de jsonPlaceHolder--------")
+
+
+getAllUser
